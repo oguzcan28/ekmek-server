70580f35ce2a46160f8a4b0b9143e707a7e06d2d
Ercode <ercode@pm.me>
2022-06-12 18:32:04 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on("logSender", log => {
+    console.log(log);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
